41385497b09a38d8693e4e5013470c3c1252fd43
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-22 18:21:19 -0600

    add template files

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Hardware configuration for my Framework 16";
+  outputs =
+    { nixos_hardware, self, ... }@inputs:
+    let
+      import_modules = import ./resources/nix/import_modules.nix;
+    in
+    {
+      inputs = inputs;
+      nixosModules.default = {
+        imports = [
+          ./hardware-configuration.nix
+        ]
+        ++ (import_modules ./settings);
+      };
+    };
+  inputs = {
+  };
+}
